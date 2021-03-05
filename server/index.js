99f0af3ceba3b0ac95df59d90df8e8b1e296a58f
Gregorie Calderon <calderon.greg6112@gmail.com>
2021-03-05 09:32:26 -0800

    Completed webpack and created index file for public

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const PORT = 3000;
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '', '')));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+
+app.listen(PORT, () => {
+  console.log('Listening')
+})
