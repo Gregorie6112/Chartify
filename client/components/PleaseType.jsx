e475210fc3d76c1e381d41844be2f8a342e7b992
Gregorie Calderon <calderon.greg6112@gmail.com>
2021-03-06 13:26:45 -0800

    Comment added for refreshing the page to see data persist

--- client/components/PleaseType.jsx
+++ client/components/PleaseType.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const PleaseType = (props) => (
   <>
-  <h1 className="typeTicker">Please type a ticker or select an existing one:</h1>
+  <h1 className="typeTicker">Please type a ticker or select an existing one:
+  <br /> Please be aware you must refresh the page after you add the ticker to see it persist.</h1>
   <input className="inputForTyping" id="typedValue" placeholder="Insert Ticker Here . . ." onKeyPress={() => props.getData(event)}></input>
   </>
 );
